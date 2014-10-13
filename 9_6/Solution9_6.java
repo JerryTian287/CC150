59780b655e0602c5b4c29e767764efc994787e50
Jerry <tjy9206@gmail.com>
2014-10-13 14:26:09 -0400

    test

--- 9_6/Solution9_6.java
+++ 9_6/Solution9_6.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+
 public class Solution9_6 {
 	public static void validParentheses(int n) {
 		List<String> result = new ArrayList<String>();
